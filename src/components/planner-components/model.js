bdc1838b317417eebafb92cf5653d5e4e31fa2d3
MuhammadAmer03 <90480247+MuhammadAmer03@users.noreply.github.com>
2025-04-26 18:54:47 -0400

    Update model.js

--- src/components/planner-components/model.js
+++ src/components/planner-components/model.js
@@ -5,16 +5,11 @@ export default class Model {
 
   getGroupedCourses() {
     return {
-      csc: this.courses.filter(c => c.requirement === "core"), // Core courses
-      mat: this.courses.filter(c => c.requirement === "math"), // Math courses
-      csc400: this.courses.filter(c => c.requirement === "elective"), // Elective courses
-      science: this.courses.filter(c => c.requirement === "science"), // Science courses
-      communication: this.courses.filter(c => c.requirement === "communication"), // Communication courses
-      fye: this.courses.filter(c => c.requirement === "FYE") // First Year Experience (FYE) courses
+      csc: this.courses.filter(c => c.id.startsWith("CSC") && !c.id.startsWith("CSC4")),
+      mat: this.courses.filter(c => c.id.startsWith("MAT")),
+      csc400: this.courses.filter(c => c.id.startsWith("CSC4")),
     };
   }
-  
-  
 
   getCourseById(id) {
     return this.courses.find(course => course.id === id);
